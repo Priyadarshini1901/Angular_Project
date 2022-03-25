60a4e1fc3148e54816c6f01c8066bebf9b48c549
Priyadarshini1901 <76025680+Priyadarshini1901@users.noreply.github.com>
2022-03-25 10:22:00 +0530

    Add files via upload

---
+++ src/app/contactus/contact-us.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-us',
+  template: `
+    <img class="im2" src="assets/images/contact.JPG">
+    <img class="im1" src="assets/images/last.JPG">
+  `,
+  styles: [
+  ]
+})
+export class ContactUsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
